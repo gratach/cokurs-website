b846124e6286fb74e4032fb5ff0b1a6b7c542eee
Patrick Richter <patrick-richter@posteo.de>
2026-03-03 16:09:18 +0100

    Fixed project multiply bug

--- lib/append_project.py
+++ lib/append_project.py
@@ -1,7 +1,7 @@
 def append_project(old_projects_list, new_project, newProjectAuthorIndexMappedToCombinedAuthorIndex):
     old_projects_list_modified = [{k:v for k, v in p.items() if not k in {"id", "von"}} for p in old_projects_list]
     new_project_modified = {k:v for k, v in new_project.items() if not k in {"id", "von"}}
-    if not new_project in old_projects_list_modified:
+    if not new_project_modified in old_projects_list_modified:
         new_project_modified["id"] = len(old_projects_list)
         if "von" in new_project:
             new_project_modified["von"] = newProjectAuthorIndexMappedToCombinedAuthorIndex[new_project["von"]]
